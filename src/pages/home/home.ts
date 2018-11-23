98e77f389f52165434f9f3ffc1db511e3cad751d
ramonparaiba <ramonparaiba@gmail.com>
2018-11-23 10:02:43 -0300

    App etanol ou gasolina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public gasolina:string;
+  public etanol:string;
+  public calculo:string;
+  public resultado:number;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  calculoCombustivel(){
+    let alcool = parseFloat(this.etanol)
+    let gas = parseFloat(this.gasolina);
+    this.resultado = alcool/gas;
+    
+    if (this.resultado<0.7){
+      this.calculo = "Etanol é Melhor";
+    } else
+    this.calculo = "Gasolina é Melhor";
   }
+
 
 }
